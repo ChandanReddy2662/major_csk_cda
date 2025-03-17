b1467a7cf2c0bad1d24bcefce57977856927a161
ChandanReddy2662 <132290886+ChandanReddy2662@users.noreply.github.com>
2025-03-17 11:47:14 +0530

    Update Chat.jsx

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 import { useAuth } from '../context/AuthContext';
-import { axios } from 'axios'
+import axios from 'axios'
 
 const VITE_SERVER = import.meta.env.VITE_API_URL
 const Chat = ({ chatId, donationId, recipientId, recipientName }) => {
